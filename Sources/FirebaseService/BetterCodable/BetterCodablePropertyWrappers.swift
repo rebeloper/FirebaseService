2cfe5c440c18312fd42622ac75d082329371d0c2
Alex Nagy <therebeloper@gmail.com>
2021-07-15 13:41:17 +0300

    DefaultTimestamp

--- Sources/FirebaseService/BetterCodable/BetterCodablePropertyWrappers.swift
+++ Sources/FirebaseService/BetterCodable/BetterCodablePropertyWrappers.swift
@@ -58,3 +58,12 @@ public struct DefaultNowStrategy: DefaultCodableStrategy {
 /// `@DefaultNow` decodes Dates and defaults the value to now if the Decoder is unable to decode the value.
 public typealias DefaultNow = DefaultCodable<DefaultNowStrategy>
 
+// MARK: - Timestamp
+public struct DefaultTimestampStrategy: DefaultCodableStrategy {
+    public static var defaultValue: Timestamp { return Timestamp() }
+}
+
+/// Decodes Timestamp defaulting to now if applicable
+///
+/// `@DefaultTimestamp` decodes Timestamps and defaults the value to now if the Decoder is unable to decode the value.
+public typealias DefaultTimestamp = DefaultCodable<DefaultTimestampStrategy>
